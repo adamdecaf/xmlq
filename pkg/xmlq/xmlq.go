c34bbce8fb0aeb7a8a6f735cf6f067ecec28b3fb
Adam Shannon <adamkshannon@gmail.com>
2024-02-07 15:11:51 -0600

    xmlq: formatting with proper indenting

--- pkg/xmlq/xmlq.go
+++ pkg/xmlq/xmlq.go
@@ -28,15 +28,8 @@ var (
 	ShowNone     MaskingType = "show-none"
 )
 
-// maxTokenLimit is the upper limit of how many XML tokens to consume.
-const maxTokenLimit = 1000
-
 func MarshalIndent(input io.Reader, opts *Options) ([]byte, error) {
-	var buf bytes.Buffer
-
-	depthLevel := -1
-	var tokensConsumed int
-	var previousAction xml.Token
+	var depthLevel int
 
 	var options Options
 	if opts != nil {
@@ -45,66 +38,100 @@ func MarshalIndent(input io.Reader, opts *Options) ([]byte, error) {
 		opts.Indent = "  "
 	}
 
-	decoder := xml.NewDecoder(input)
-	for {
-		token, err := decoder.Token()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-		}
-		tokensConsumed += 1
-		if tokensConsumed >= maxTokenLimit {
-			return nil, fmt.Errorf("depth of xml document is too deep at %d levels", tokensConsumed)
-		}
-		if token == nil {
-			continue
-		}
+	var buf bytes.Buffer
+	data := make([]byte, 32)
+	size := len(data)
 
-		switch elm := token.(type) {
-		case xml.StartElement:
-			depthLevel += 1
+	var insideStartTag bool
+	var insideEndTag bool
+	var doubleNewLine bool
 
-			// Write a newline after two StartElements are encountered in a row
-			if _, ok := previousAction.(xml.StartElement); ok {
-				buf.WriteString("\n")
-			}
+read:
+	n, err := io.ReadFull(input, data)
+	if err == io.EOF {
+		goto done
+	}
+	size = n
 
-			padding := fmt.Sprintf("%s%s", options.Prefix, strings.Repeat(options.Indent, depthLevel))
-			innerElement := elm.Name.Local
-			if elm.Name.Space != "" {
-				innerElement = fmt.Sprintf("%s:%s", elm.Name.Space, elm.Name.Local)
-			}
-			buf.WriteString(fmt.Sprintf("%s<%s>", padding, innerElement))
-
-			// Save token for next iteration
-			previousAction = elm
-
-		case xml.CharData:
-			var value string
-			switch prev := previousAction.(type) {
-			case xml.StartElement:
-				value = applyMaskIfNeeded(prev.Name, string(elm))
-
-			case xml.EndElement:
-				value = applyMaskIfNeeded(prev.Name, string(elm))
-			}
-			buf.WriteString(strings.TrimSpace(value))
-
-		case xml.EndElement:
-			innerElement := elm.Name.Local
-			if elm.Name.Space != "" {
-				innerElement = fmt.Sprintf("%s:%s", elm.Name.Space, elm.Name.Local)
-			}
-
-			buf.WriteString(fmt.Sprintf("</%s>\n", innerElement))
-
-			// Save token for next iteration
-			previousAction = elm
-			depthLevel -= 1
-		}
+	// < is the last character, so return it to the input
+	if data[size-1] == '<' {
+		size -= 1
+		input = io.MultiReader(strings.NewReader("<"), input)
 	}
 
+	for i := 0; i < size; i++ {
+		switch data[i] {
+		case '<':
+			if i+1 >= size {
+				// There's nothing else read in the buffer
+				panic("bad thing")
+			}
+
+			// Check what tag is coming
+			i += 1
+			switch data[i] {
+			case '/':
+				// end tag
+				insideStartTag = false
+				insideEndTag = true
+				buf.WriteString("</")
+
+			case '?':
+				// XML header, do nothing
+				insideEndTag = true
+				buf.WriteString("<?")
+
+			default:
+				// start tag
+				if insideStartTag {
+					doubleNewLine = doubleNewLine && (insideStartTag && !insideEndTag)
+					if !doubleNewLine {
+						buf.WriteString("\n")
+
+						padding := fmt.Sprintf("%s%s", options.Prefix, strings.Repeat(options.Indent, depthLevel))
+						if padding != "" {
+							buf.WriteString(padding)
+						}
+					}
+				}
+				insideStartTag = true
+				depthLevel += 1
+				buf.WriteString("<")
+				buf.Write(data[i : i+1])
+			}
+
+		case '/':
+			buf.WriteString("/")
+			insideStartTag = false
+			insideEndTag = true
+
+		case '>':
+			buf.WriteString(">")
+			if !insideStartTag && insideEndTag {
+				buf.WriteString("\n")
+
+				depthLevel -= 1
+				if depthLevel < 0 {
+					depthLevel = 0
+				}
+
+				padding := fmt.Sprintf("%s%s", options.Prefix, strings.Repeat(options.Indent, depthLevel))
+				if padding != "" {
+					buf.WriteString(padding)
+				}
+			}
+			insideEndTag = false
+
+		case '\n', '\r':
+			// skip newlines since we add them
+
+		default:
+			buf.Write(data[i : i+1])
+		}
+	}
+	goto read
+
+done:
 	return buf.Bytes(), nil
 }
 
